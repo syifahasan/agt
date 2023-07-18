08fa1719c9292a54614e7f7af53758a369eea319
asepsj <asepsj07@gmail.com>
2023-07-18 15:12:14 +0700

    zero

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,20 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'HomePage AGT',
-      theme: ThemeData(
-        fontFamily: 'SFProDisplay',
-      ),
-      home: HomePage(),
-    );
-  }
-}
-
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
