ab69e37c88f43d9980fed422e52de3459e1be65e
asepsj <asepsj07@gmail.com>
2023-08-07 16:41:09 +0700

    edit profile

--- lib/pages/editProfile.dart
+++ lib/pages/editProfile.dart
@@ -6,75 +6,187 @@ class editProfile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        iconTheme: IconThemeData(
-          color: Colors.white,
-        ),
-      ),
-      body: ListView(
-        children: [
-          Stack(
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height,
-                padding: EdgeInsets.only(left: 40),
-                decoration: BoxDecoration(color: Colors.black),
+    final mediaQueryData = MediaQuery.of(context);
+    final screenWidth = mediaQueryData.size.width;
+    final screenHeight = mediaQueryData.size.height;
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: Colors.black,
+      body: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: Colors.transparent,
+            // Properties for the AppBar
+            expandedHeight:
+                screenHeight * 0.12, // Set the desired height of the AppBar
+            pinned: false, // Set this to false to make AppBar non-sticky
+            flexibleSpace: Container(
+              margin: EdgeInsets.only(top: 100, left: 30),
+              height: screenHeight * 20 / 100,
+              // A container for the flexible space (background) of the AppBar
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: Container(
+                padding: EdgeInsets.only(),
                 child: Text(
                   'EDIT PROFILE',
                   style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.w700,
-                  ),
+                      color: Colors.white,
+                      fontSize: screenWidth * 0.09,
+                      fontWeight: FontWeight.bold),
                 ),
               ),
-              ClipPath(
-                clipper: MyClipPath(),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 150,
-                          left: 30,
-                          right: 30,
-                          bottom: 40,
+            ),
+          ),
+          // SliverList to contain the scrollable content
+          SliverToBoxAdapter(
+            child: ClipPath(
+              clipper: MyClipPath(),
+              child: Container(
+                padding: EdgeInsets.only(
+                  top: 90,
+                  left: 30,
+                  right: 30,
+                ),
+                width: screenWidth,
+                height: screenHeight,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 80,
+                      child: Container(
+                        width: screenWidth * 0.84,
+                        height: screenHeight * 0.7,
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.all(Radius.circular(50)),
                         ),
                         child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.grey,
-                          ),
-                          child: RichText(
-                            text: TextSpan(
-                              text:
-                                  'How would you like to top up AGCoin Balance?',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize:
-                                    MediaQuery.of(context).size.width * 0.065,
-                                fontWeight: FontWeight.w500,
+                          padding:
+                              EdgeInsets.only(top: 70, left: 20, right: 20),
+                          child: Column(
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                child: _formProfile(
+                                  title: 'Name',
+                                  value: 'Asep Saefuddin Zuhri',
+                                  hint: 'Full Name',
+                                ),
                               ),
-                            ),
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                child: _formProfile(
+                                  title: 'Phone Number',
+                                  value: '+62 000 0000 0000',
+                                  hint: 'Phone Number',
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                child: _formProfile(
+                                  title: 'Email',
+                                  value: 'asep@gamil.com',
+                                  hint: 'Email',
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                child: _formProfile(
+                                  title: 'Address',
+                                  value: 'Indramayu - Kertasemaya',
+                                  hint: 'Address',
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: SizedBox(
+                        width: 129,
+                        height: 129,
+                        child: Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            Container(
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: AssetImage(
+                                    'assets/other/profile1.png',
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 0,
+                              right: 0,
+                              child: CircleAvatar(
+                                radius: 20,
+                                backgroundColor:
+                                    Theme.of(context).scaffoldBackgroundColor,
+                                child: Container(
+                                  margin: const EdgeInsets.all(8.0),
+                                  decoration: const BoxDecoration(
+                                      color: Colors.green,
+                                      shape: BoxShape.circle),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ],
+            ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class _formProfile extends StatelessWidget {
+  final String title;
+  final String value;
+  final String hint;
+  const _formProfile({
+    required this.title,
+    required this.value,
+    required this.hint,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          alignment: Alignment.topLeft,
+          child: Text(
+            title,
+            style: TextStyle(fontWeight: FontWeight.w700),
+          ),
+        ),
+        TextFormField(
+          initialValue: value,
+          decoration: InputDecoration(
+            hintText: hint,
+          ),
+        )
+      ],
     );
   }
 }
