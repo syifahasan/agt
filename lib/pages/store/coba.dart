9604d76b90a87f031b957901bf10d1f8496e6e92
asepsj <asepsj07@gmail.com>
2023-09-11 14:21:18 +0700

    page blashsale resolve

--- lib/pages/store/coba.dart
+++ lib/pages/store/coba.dart
@@ -4,21 +4,20 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'dart:async';
-import 'package:get/get.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 
-class BlastSalesPage extends StatefulWidget {
+import 'package:get/get.dart';
+
+class Coba extends StatefulWidget {
   @override
-  _BlastSalesPageState createState() => _BlastSalesPageState();
+  _CobaState createState() => _CobaState();
 }
 
-class _BlastSalesPageState extends State<BlastSalesPage> {
+class _CobaState extends State<Coba> {
   // Daftar tampilan
   List<Widget> contents = [
     tampilan1(),
     tampilan2(),
     tampilan3(),
-    tampilan4(),
   ];
 
   // Indeks tampilan saat ini
@@ -110,200 +109,6 @@ class _BlastSalesPageState extends State<BlastSalesPage> {
             ],
           ),
         ],
-      ),
-    );
-  }
-}
-
-class SaleItems extends StatefulWidget {
-  const SaleItems({
-    super.key,
-    required this.itemname,
-    required this.discount,
-    required this.price,
-    required this.desc,
-  });
-
-  final String itemname;
-  final String discount;
-  final String price;
-  final String desc;
-
-  @override
-  State<SaleItems> createState() => _SaleItemsState();
-}
-
-class _SaleItemsState extends State<SaleItems> {
-  @override
-  Widget build(BuildContext context) {
-    final w = MediaQuery.of(context).size.width;
-    return Container(
-      padding: EdgeInsets.only(top: w * 0.02, left: w * 0.03, right: w * 0.03),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            width: w * 0.28,
-            height: w * 0.30,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              image: DecorationImage(
-                  fit: BoxFit.contain,
-                  image: AssetImage(
-                      'assets/icons/store/fashionsImages/butterflyhoodie.png')),
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                    color: Color(0xffD9d9d9),
-                    offset: Offset(0, 3),
-                    blurRadius: 4,
-                    spreadRadius: 1),
-              ],
-            ),
-          ),
-          Container(
-            width: w * 0.45,
-            height: w * 0.30,
-            decoration: BoxDecoration(),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  widget.itemname,
-                  style: TextStyle(
-                    fontSize: w * 0.05,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                Text(
-                  'Garment dyed Hooide',
-                  style: TextStyle(fontSize: w * 0.04),
-                ),
-                Text(
-                  widget.discount,
-                  style: TextStyle(
-                    decoration: TextDecoration.lineThrough,
-                    fontSize: w * 0.03,
-                  ),
-                ),
-                Text(
-                  widget.price,
-                  style: TextStyle(fontSize: w * 0.04, color: Colors.red),
-                ),
-                Container(
-                  width: w * 0.45,
-                  height: w * 0.04,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(w * 0.5),
-                    border: Border.all(color: Colors.grey),
-                  ),
-                  child: Center(
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        LinearProgressIndicator(
-                          value: 1,
-                          backgroundColor: Colors.transparent,
-                          borderRadius: BorderRadius.circular(w * 0.05),
-                          minHeight: w * 0.035,
-                          valueColor:
-                              AlwaysStoppedAnimation<Color>(Colors.grey),
-                        ),
-                        Text('70%',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: w * 0.03,
-                                fontWeight: FontWeight.bold)),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: w * 0.009,
-                ),
-                Text(
-                  widget.desc,
-                  style: TextStyle(
-                    fontSize: w * 0.035,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: w * 0.15,
-            height: w * 0.09,
-            child: TextButton(
-              onPressed: () {},
-              child: Text(
-                'BUY',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: w * 0.04,
-                ),
-              ),
-            ),
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(w * 0.02),
-              boxShadow: [
-                BoxShadow(
-                  color: Color(0xffD9d9d9),
-                  offset: Offset(0, 3),
-                  blurRadius: w * 0.04,
-                  spreadRadius: w * 0.01,
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class FlashSaleTimerWidget extends StatefulWidget {
-  final DateTime endTime;
-
-  FlashSaleTimerWidget({required this.endTime});
-
-  @override
-  _FlashSaleTimerWidgetState createState() => _FlashSaleTimerWidgetState();
-}
-
-class _FlashSaleTimerWidgetState extends State<FlashSaleTimerWidget> {
-  late Timer _timer;
-  Duration _duration = Duration();
-
-  @override
-  void initState() {
-    super.initState();
-    _duration = widget.endTime.difference(DateTime.now());
-    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        _duration = widget.endTime.difference(DateTime.now());
-        if (_duration.isNegative) {
-          _timer.cancel();
-        }
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final w = MediaQuery.of(context).size.width;
-    return Text(
-      "${_duration.inHours.toString().padLeft(2, '0')} : ${_duration.inMinutes.remainder(60).toString().padLeft(2, '0')} : ${_duration.inSeconds.remainder(60).toString().padLeft(2, '0')}",
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: w * 0.045,
-        color: Colors.white,
       ),
     );
   }
@@ -411,59 +216,186 @@ class tampilan3 extends StatelessWidget {
   }
 }
 
-class tampilan4 extends StatelessWidget {
-  const tampilan4({
+class SaleItems extends StatefulWidget {
+  const SaleItems({
     super.key,
+    required this.itemname,
+    required this.discount,
+    required this.price,
+    required this.desc,
   });
+
+  final String itemname;
+  final String discount;
+  final String price;
+  final String desc;
+
+  @override
+  State<SaleItems> createState() => _SaleItemsState();
+}
+
+class _SaleItemsState extends State<SaleItems> {
+  double _progressValue = 0.7; // Initial value
+
+  void _updateProgress(double newValue) {
+    setState(() {
+      _progressValue = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final w = MediaQuery.of(context).size.width;
     return Container(
-      child: Column(
+      child: Row(
         children: [
-          SaleItems(
-            itemname: 'BUTTERFLY HOODIE',
-            discount: 'Rp 799.999.99',
-            price: 'Rp 499.999.999',
-            desc: '28 Sold',
+          Container(
+            width: w * 0.28,
+            height: w * 0.30,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              image: DecorationImage(
+                  fit: BoxFit.contain,
+                  image: AssetImage(
+                      'assets/icons/store/fashionsImages/butterflyhoodie.png')),
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                    color: Color(0xffD9d9d9),
+                    offset: Offset(0, 3),
+                    blurRadius: 4,
+                    spreadRadius: 1),
+              ],
+            ),
           ),
-          SaleItems(
-            itemname: 'PANCOAT HOODIE',
-            discount: 'Rp 699.999.999',
-            price: 'Rp 399.399.999',
-            desc: 'Limited Stok',
+          Container(
+            margin: EdgeInsets.only(left: 7),
+            padding: EdgeInsets.only(bottom: 5),
+            width: w * 0.45,
+            height: w * 0.30,
+            decoration: BoxDecoration(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.itemname,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Garment dyed Hooide',
+                  style: TextStyle(fontSize: 12),
+                ),
+                Text(
+                  widget.discount,
+                  style: TextStyle(
+                    decoration: TextDecoration.lineThrough,
+                    fontSize: 10,
+                  ),
+                ),
+                Text(
+                  widget.price,
+                ),
+                Container(
+                  width: 200,
+                  height: 10,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(color: Colors.grey),
+                  ),
+                  child: LinearProgressIndicator(
+                    value: _progressValue,
+                    backgroundColor: Colors.transparent,
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  widget.desc,
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+              ],
+            ),
           ),
-          SaleItems(
-            itemname: 'BUTTERFLY HOODIE',
-            discount: 'Rp 799.999.99',
-            price: 'Rp 499.999.999',
-            desc: '28 Sold',
-          ),
-          SaleItems(
-            itemname: 'PANCOAT HOODIE',
-            discount: 'Rp 699.999.999',
-            price: 'Rp 399.399.999',
-            desc: 'Limited Stok',
-          ),
-          SaleItems(
-            itemname: 'PANCOAT HOODIE',
-            discount: 'Rp 699.999.999',
-            price: 'Rp 399.399.999',
-            desc: 'Limited Stok',
-          ),
-          SaleItems(
-            itemname: 'PANCOAT HOODIE',
-            discount: 'Rp 699.999.999',
-            price: 'Rp 399.399.999',
-            desc: 'Limited Stok',
-          ),
-          SaleItems(
-            itemname: 'PANCOAT HOODIE',
-            discount: 'Rp 699.999.999',
-            price: 'Rp 399.399.999',
-            desc: 'Limited Stok',
-          ),
+          Container(
+            margin: EdgeInsets.only(left: 10),
+            width: w * 0.15,
+            height: w * 0.09,
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                'BUY',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    color: Color(0xffD9d9d9),
+                    offset: Offset(0, 3),
+                    blurRadius: 4,
+                    spreadRadius: 1),
+              ],
+            ),
+          )
         ],
+      ),
+    );
+  }
+}
+
+class FlashSaleTimerWidget extends StatefulWidget {
+  final DateTime endTime;
+
+  FlashSaleTimerWidget({required this.endTime});
+
+  @override
+  _FlashSaleTimerWidgetState createState() => _FlashSaleTimerWidgetState();
+}
+
+class _FlashSaleTimerWidgetState extends State<FlashSaleTimerWidget> {
+  late Timer _timer;
+  Duration _duration = Duration();
+
+  @override
+  void initState() {
+    super.initState();
+    _duration = widget.endTime.difference(DateTime.now());
+    _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        _duration = widget.endTime.difference(DateTime.now());
+        if (_duration.isNegative) {
+          _timer.cancel();
+        }
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final w = MediaQuery.of(context).size.width;
+    return Text(
+      "${_duration.inHours.toString().padLeft(2, '0')} : ${_duration.inMinutes.remainder(60).toString().padLeft(2, '0')} : ${_duration.inSeconds.remainder(60).toString().padLeft(2, '0')}",
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: w * 0.045,
+        color: Colors.white,
       ),
     );
   }
